96dfbfbc545125209c97f84761ac261e5e67abba
crisostomi <donatocrisostomi@gmail.com>
2019-09-07 20:42:30 +0200

    add reset method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
+import DataTypes.PreconditionsException;
 import Model.*;
 import ModelicaSimulableModel.*;
+import Parser.FormatNotSupportedException;
 import Util.CustomLogger;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.TransformerException;
+import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,7 +17,7 @@ public class Main {
     public static final String ABUNDANCES_FILENAME = "abundances.tsv";
     public static final String REACTOME_FILENAME = "pathway.sbml";
     public static final String LOG_FILENAME = "log.txt";
-    public static final String TEST = "urea";
+    public static final String TEST = "sumoylation";
 
     public static final double HeLaProteins = 2.3e9;
     public static final double minInitialAmount = 0;
@@ -25,19 +29,31 @@ public class Main {
     public static final double minKm = 1e-8;
     public static final double maxKm = 1;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException, IOException, PreconditionsException, FormatNotSupportedException, SAXException, XMLStreamException {
+        test(TEST);
+        System.out.println("All done!");
+    }
 
+    public static void reset(String folder) throws ParserConfigurationException, TransformerException, IOException, PreconditionsException, FormatNotSupportedException, SAXException, XMLStreamException {
+        File folderFile = new File(folder);
+        for(String file: folderFile.list()){
+            test(file);
+        }
+    }
+
+    public static void test(String test) throws SAXException, PreconditionsException, IOException, XMLStreamException, ParserConfigurationException, FormatNotSupportedException, TransformerException {
         String username = System.getProperty("user.name");
-        String projectFolder = "/home/"+username+"/Dropbox/Tesisti/software";
-        String testFolder = projectFolder + "/test-cases/"+TEST;
-        String kbPath = testFolder + "/in/"+REACTOME_FILENAME;
-        String globalAbundancesPath = projectFolder +"/test-cases/"+ ABUNDANCES_FILENAME;
-        String logPath = testFolder +"/out/"+ LOG_FILENAME;
-        String localAbundancesPath = testFolder+"/in/"+ABUNDANCES_FILENAME;
+        String projectFolder = "/home/" + username + "/Dropbox/Tesisti/software";
+        String knowledgeFolder = projectFolder + "/knowledge";
+        String testFolder = projectFolder + "/test-cases/" + test;
+        String kbPath = testFolder + "/in/" + REACTOME_FILENAME;
+        String globalAbundancesPath = knowledgeFolder + "/test-cases/" + ABUNDANCES_FILENAME;
+        String logPath = testFolder + "/out/" + LOG_FILENAME;
+        String localAbundancesPath = testFolder + "/in/" + ABUNDANCES_FILENAME;
         String dumpPath = testFolder + "/out/model_dump.xml";
         String xmlPath = testFolder + "/in/quantitative.xml";
 
-        System.out.println("Model2Simulable: testing test-case "+TEST);
+        System.out.println("Model2Simulable: testing test-case " + test);
         CustomLogger.setup(logPath);
 
         try {
@@ -63,12 +79,8 @@ public class Main {
             Model m_loaded = Model.load(dumpPath);
             ModelicaSimulableModel msm = HandleModelica.buildSimulableModel(m_loaded);
             HandleModelica.buildModelica(msm, testFolder + "/out");
-
-            System.out.println("All done!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
+        } catch (XMLStreamException | IOException | FormatNotSupportedException | PreconditionsException | ParserConfigurationException | SAXException | TransformerException ex) {
+            ex.printStackTrace();
         }
     }
 }
